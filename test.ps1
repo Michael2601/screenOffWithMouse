65a94d305c6922960b369572d8121eb9fcae6116
Michael2601 <49235734+Michael2601@users.noreply.github.com>
2020-01-16 15:41:59 +0300

    Add worked files

---
+++ test.ps1
@@ -0,0 +1,4 @@
+﻿Disable-PnpDevice -InstanceId "HID\VID_046D&PID_C077\7&194B0A87&0&0000" -confirm:$false
+scrnsave.scr
+[void][System.Console]::ReadKey($FALSE)
+Enable-PnpDevice -InstanceId "HID\VID_046D&PID_C077\7&194B0A87&0&0000" -confirm:$false
