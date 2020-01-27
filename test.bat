e9da84bfd8bbf4bd8ec58a8255c838694747ef7b
Mikhail Sheshilov <sheshilov@acdlabs.by>
2020-01-27 19:33:20 +0300

    Update with non-lock opportunity

--- test.bat
+++ test.bat
@@ -3,3 +3,4 @@ for /F "tokens=2" %%i in ('date /t') do set mydate=%%i
 set mytime=%time%
 echo Current time is %mydate%:%mytime%
 PowerShell.exe -Command "& '%~dpn0.ps1'"
+pause
--- test.ps1
+++ test.ps1
@@ -1,4 +1,21 @@
-﻿Disable-PnpDevice -InstanceId "HID\VID_046D&PID_C077\7&194B0A87&0&0000" -confirm:$false
+﻿Add-Type -AssemblyName System.Windows.Forms
+
+$sleepTimeInSeconds = 1
+$devicePath = "HID\VID_046D&PID_C077\7&194B0A87&0&0000"
+
+function pressAlt([int]$sleep){     
+	while($true)
+        {
+        Write-Host "Press Q to exit"  
+        $Q=$HOST.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+		sleep -Seconds $sleep
+		[System.Windows.Forms.SendKeys]::SendWait("%")   
+		if($Q.character -eq 'q'){break}
+        }
+}
+
+Disable-PnpDevice -InstanceId $devicePath -confirm:$false
 scrnsave.scr
-[void][System.Console]::ReadKey($FALSE)
-Enable-PnpDevice -InstanceId "HID\VID_046D&PID_C077\7&194B0A87&0&0000" -confirm:$false
+[System.Windows.Forms.SendKeys]::SendWait("%")  
+pressAlt $sleepTimeInSeconds
+Enable-PnpDevice -InstanceId $devicePath -confirm:$false
