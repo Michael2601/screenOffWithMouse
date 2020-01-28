1402f21e98cd68ef436b9ae1cd538fdce6464c45
Mikhail Sheshilov <sheshilov@acdlabs.by>
2020-01-28 16:34:05 +0300

    Simplified installation procedure

--- test.bat
+++ test.bat
@@ -2,5 +2,5 @@
 for /F "tokens=2" %%i in ('date /t') do set mydate=%%i
 set mytime=%time%
 echo Current time is %mydate%:%mytime%
-PowerShell.exe -Command "& '%~dpn0.ps1'"
+PowerShell.exe -ep Bypass "& '%~dpn0.ps1'"
 pause
